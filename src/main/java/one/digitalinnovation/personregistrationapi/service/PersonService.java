afe364910a0f240a1c450978927d5df4b91ad6fb
anderrf <anderson_rocha_farias@outlook.com>
2021-07-27 17:41:30 -0300

    Person saving by repository save transferred to service

---
+++ src/main/java/one/digitalinnovation/personregistrationapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.personregistrationapi.service;
+
+import one.digitalinnovation.personregistrationapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personregistrationapi.entity.Person;
+import one.digitalinnovation.personregistrationapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
