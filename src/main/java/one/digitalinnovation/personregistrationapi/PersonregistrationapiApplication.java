dd5ec9bd7c6f70da13a5cee74fd17dfc710afe44
anderrf <anderson_rocha_farias@outlook.com>
2021-07-27 14:51:30 -0300

    Start of project

---
+++ src/main/java/one/digitalinnovation/personregistrationapi/PersonregistrationapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonregistrationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonregistrationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/personregistrationapi/PersonregistrationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonregistrationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
