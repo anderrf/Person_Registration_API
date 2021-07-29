7fa39041f9bcf88ad15ee58a797bf3ad9869dad0
anderrf <anderson_rocha_farias@outlook.com>
2021-07-29 15:08:28 -0300

    Listing of all people

--- src/main/java/one/digitalinnovation/personregistrationapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personregistrationapi/service/PersonService.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
-    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
@@ -20,11 +22,18 @@ public class PersonService {
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
-        Person personToSave = personMapper.toModel(personDTO);
+        Person personToSave = PersonMapper.INSTANCE.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(PersonMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
     }
 }
